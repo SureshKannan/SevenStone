29f0a1a77116707bf0e2e481d1dd6679a8e87550
SureshKannan <ksuriyaa@hotmail.com>
2014-07-09 07:36:46 -0400

    First Set

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SevenStone::Application.config.secret_key_base = '57d002e5c5917968ad8e5ff001aea1be43717d0816280a6c4c1e47ed06b1a03fc04b85deae19950c7cf1cf41ce53c793cfbd964ac8c6a847b8f08b76c56fc998'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SevenStone::Application.config.session_store :cookie_store, key: '_SevenStone_session'
